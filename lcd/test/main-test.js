b9d39cff3ff64e271982a77c194ee3f114b08c92
Max-Hu <huzhiwise@gmail.com>
2016-03-02 15:53:41 +0800

    create split number task

--- lcd/test/main-test.js
+++ lcd/test/main-test.js
@@ -9,8 +9,8 @@ describe('Main Task: print lcd', function() {
     input = '910';
 
     output = '._. ... ._.\n' +
-              '|_| ..| |.|\n' +
-              '..| ..| |_|';
+      '|_| ..| |.|\n' +
+      '..| ..| |_|';
 
   });
 
@@ -21,4 +21,24 @@ describe('Main Task: print lcd', function() {
     expect(console.log).toHaveBeenCalledWith(output);
 
   });
+});
+describe('Task 1: split number', function() {
+
+  var input;
+  var output;
+
+  beforeEach(function () {
+
+    input = '910';
+
+    output = ['9','1','0'];
+
+  });
+
+  it('should get a split array', function () {
+    var result = splitNumber(input);
+    expect(result).toEqual(output);
+  });
+
+
 });
