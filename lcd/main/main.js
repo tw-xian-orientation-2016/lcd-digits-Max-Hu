faa5a96c9e63c1532a566b3e7ebf853c4af34708
Max-Hu <huzhiwise@gmail.com>
2016-03-02 15:58:45 +0800

    implement get element task

--- lcd/main/main.js
+++ lcd/main/main.js
@@ -1,3 +1,11 @@
 function splitNumber(numberStr){
   return numberStr.match(/.{1,1}/g);
 }
+
+function getElement(numberArray,dictionary){
+  var charNumbers = [];
+  numberArray.forEach(function(element){
+    charNumbers.push(dictionary[parseInt(element)]);
+  });
+  return charNumbers;
+}
